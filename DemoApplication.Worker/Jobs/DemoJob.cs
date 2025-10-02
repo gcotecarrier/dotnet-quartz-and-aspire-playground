576ecdf83bba0acb1ef2afa168f3ed4d1c2a21a7
Gabriel Cote-Carrier <gcotecarrier@gmail.com>
2025-10-02 14:45:21 -0400

    Add an up-down counter for jobs active

--- DemoApplication.Worker/Jobs/DemoJob.cs
+++ DemoApplication.Worker/Jobs/DemoJob.cs
@@ -5,13 +5,15 @@ namespace DemoApplication.Worker.Jobs;
 public class DemoJob(ILogger<DemoJob> logger): IJob
 {
     public static JobKey Key = new("Demo", "Job");
+    private static Random random = new();
     
-    public Task Execute(IJobExecutionContext context)
+    public async Task Execute(IJobExecutionContext context)
     {
         context.MergedJobDataMap.TryGetString("message", out var message);
         message ??= "default";
+
+        await Task.Delay(random.Next(3000, 4000));
         
         logger.LogInformation("Hello from Quartz job {job} and message {message}", context.FireInstanceId, message);
-        return Task.CompletedTask;
     }
 }
--- DemoApplication.Worker/Metrics/JobMetrics.cs
+++ DemoApplication.Worker/Metrics/JobMetrics.cs
@@ -7,6 +7,7 @@ public class JobMetrics
         private readonly Counter<int> _jobsStarted;
         private readonly Counter<int> _jobsFinished;
         private readonly Counter<int> _jobsFailed;
+        private readonly UpDownCounter<int> _jobsActive;
 
         public JobMetrics(IMeterFactory meterFactory)
         {
@@ -14,17 +15,21 @@ public class JobMetrics
             _jobsStarted = meter.CreateCounter<int>("demoapp.jobs.started");
             _jobsFinished = meter.CreateCounter<int>("demoapp.jobs.finished");
             _jobsFailed = meter.CreateCounter<int>("demoapp.jobs.failed");
-
+            _jobsActive = meter.CreateUpDownCounter<int>("demoapp.jobs.active");
         }
 
         public void JobStarted(Type jobType)
         {
-            _jobsStarted.Add(1, new KeyValuePair<string, object?>("job.type",jobType.Name));
+            var context = new KeyValuePair<string, object?>("job.type",jobType.Name);
+            _jobsStarted.Add(1, context);
+            _jobsActive.Add(1, context);
         }
         
         public void JobFinished(Type jobType)
         {
-            _jobsFinished.Add(1, new KeyValuePair<string, object?>("job.type",jobType.Name));
+            var context = new KeyValuePair<string, object?>("job.type",jobType.Name);
+            _jobsFinished.Add(1, context);
+            _jobsActive.Add(-1, context);
         }
         
         public void JobFailed(Type jobType)
